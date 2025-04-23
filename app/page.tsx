83bfecc15abe4cafc368686bc4bf645b294d3cb5
Algik0 <algiko.llk@gmail.com>
2025-04-23 12:10:44 +0200

    Added picture

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import Login from "./components/login";
 import Register from "./components/register";
 import Dashboard from "./components/dashboard";
+import taskademiaLogo from "./images/taskademia.png";
 
 export default function Home() {
   const [isRegistering, setIsRegistering] = useState(false); // Zustand für den Registrierungsstatu
@@ -16,6 +18,12 @@ if (isLoggedIn) {
 
   return (
     <div className="min-h-screen p-8 bg-gray-100 flex flex-col items-center">
+      {/* Logo der Anwendung */}
+      <div className="absolute top-4 right-4">
+        <Image src={taskademiaLogo} alt="Taskademia Logo" width={300} height={300} className="rounded-full"/>
+      </div>
+
+      {/* Titel der Anwendung */}
       <h1 className="text-4xl font-bold mb-8">
         Taskademia {isRegistering ? "Registrieren" : "Login"}
       </h1>
