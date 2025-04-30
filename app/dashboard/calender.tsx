a6a94223eb4896dbd2f2cf74c7501e9fdc07bb84
Algik0 <algiko.llk@gmail.com>
2025-04-30 19:44:29 +0200

    Added routing for Dashboard

---
+++ app/dashboard/calender.tsx
@@ -0,0 +1,12 @@
+import BottomTabBar from "./bottomtabbar";
+
+export default function KalenderSeite() {
+    return (
+      <div className="min-h-screen pb-20 bg-gray-50 p-8">
+        <h1 className="text-3xl font-bold mb-4">📅 Kalender</h1>
+        <p>Hier wird später dein Kalender angezeigt.</p>
+        <BottomTabBar />
+      </div>
+    );
+  }
+  
